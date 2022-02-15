e7d6b995a37b7bc897fc4c49a68840cf1579c45b
win <win@WIN-PC>
2022-02-15 12:51:25 +0200

    Шелест: добавление спецификации для IDBEntry, Component и Model

--- DatabaseInteraction/Component.cs
+++ DatabaseInteraction/Component.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿//      -= PCCI =-
+// Проект команды №3
+// Группа: ПВ-011
+// Компьютерная Академия "ШАГ", 2022
+
+using System;
 
 namespace PCCI.DatabaseInteraction
 {
-    public class Component
+    /// <summary>
+    /// Класс-представление таблицы Components.
+    /// Отображает собой одну строчку из указанной таблицы.
+    /// </summary>
+    public sealed class Component : IDBEntry
     {
+        // Идентификатор строки в таблице.
+        public int Id { get; private set; }
+        // Название комплектующей.
+        public string Name { get; private set; }
+        // Путь к связанному изображению.
+        public string ImagePath { get; private set; }
+        // Описание комплектующей.
+        public string Description { get; private set; }
+        // Ссылка на сайт с дополнительной информацией.
+        public string InfoLink { get; private set; }
+        // Явзялется ли комплектующая обязательной.
+        public bool IsNecessary { get; private set; }
+
+        /// <summary>
+        /// Конструктор класса Component.
+        /// Внутри конструктора инициализировать значения полей объекта.
+        /// </summary>
+        /// <param name="values">Массив значений для их присваивания полям.</param>
+        public Component(object[] values) : base(values)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- DatabaseInteraction/Model.cs
+++ DatabaseInteraction/Model.cs
@@ -1,8 +1,41 @@
-﻿using System;
+﻿//      -= PCCI =-
+// Проект команды №3
+// Группа: ПВ-011
+// Компьютерная Академия "ШАГ", 2022
+
+using System;
 
 namespace PCCI.DatabaseInteraction
 {
-    public class Model
+    public sealed class Model : IDBEntry
     {
+        // Идентификатор строчки в таблице.
+        public int Id { get; private set; }
+        // Идентификатор типа комплектующего в таблице Components.
+        public int ComponentId { get; private set; }
+        // Название модели.
+        public string Name { get; private set; }
+        // Производитель модели.
+        public string Manufacturer { get; private set; }
+        // Год выпуска модели.
+        public DateTime ReleaseYear { get; private set; }
+        // Средняя цена на модель (в долларах США).
+        public float AvgPrice { get; private set; }
+        // Путь к связанному изображению.
+        public string ImagePath { get; private set; }
+        // Характеристики модели
+        public string Characteristics { get; private set; }
+        // Ссылка на сайт с дополнительной информацией.
+        public string InfoLink { get; private set; }
+
+        /// <summary>
+        /// Конструктор класса Model.
+        /// Внутри конструктора инициализировать значения полей объекта.
+        /// </summary>
+        /// <param name="values">Массив значений для их присваивания полям.</param>
+        public Model(object[] values) : base(values)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
